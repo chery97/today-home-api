57406f3020b61bdebaeab3e906c602e12f37ca89
myeongje <myeong8730@gmail.com>
2023-12-11 23:21:38 +0900

    userModule, userController 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ormConfig } from "./orm.config";
+import { UserModule } from './api/user/user.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRootAsync({ useFactory: ormConfig })
+    TypeOrmModule.forRootAsync({ useFactory: ormConfig }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
