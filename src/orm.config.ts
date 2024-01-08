4cc225b9aa6f8b89730060ba1ddbe8f4ecbaef71
Jay <jay@geekstudio.co.kr>
2024-01-08 19:37:32 +0900

    DB local -> EC2로 변경

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -10,10 +10,10 @@ function ormConfig(): TypeOrmModuleOptions {
   const ormconfig: TypeOrmModuleOptions = {
     type: 'mysql',
     database: 'today_home',
-    host: 'localhost',
+    host: '15.164.78.211',
     port: 3306,
     username: 'root',
-    password: 'rkdaudwp123!',
+    password: 'geek2020',
     logging: true,
     synchronize: commonConf.SYNCRONIZE,
     entities: commonConf.ENTITIES,
