54f056e11d179c2cf558d0be78691dc69aecaee6
Jay <Jay@geekstudio.co.kr>
2024-01-14 23:39:11 +0900

    유저 데이터 임시 추가

--- src/api/user/user.service.ts
+++ src/api/user/user.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { user } from "./entity/user.entity";
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private readonly users = [
+    {
+      id: 1,
+      userName: '강명제',
+      password: 'changeme',
+      userPhone: '010-4032-8730',
+      userMail: 'changeme',
+    },
+    {
+      id: 2,
+      userName: '강명제1',
+      password: 'guess',
+      userPhone: '010-4032-8730',
+      userMail: 'guess',
+    },
+  ];
+
+  async findOne(userName: string): Promise<user | undefined> {
+    return this.users.find(user => user.userName === userName);
+  }
+}
